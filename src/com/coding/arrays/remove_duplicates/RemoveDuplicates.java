a7d5bb1b978d38c2f48210c8cab8d780f2cb14f6
Andres <andresmerida1@gmail.com>
2023-02-26 00:32:23 -0400

    adding new function with for loop

--- src/com/coding/arrays/remove_duplicates/RemoveDuplicates.java
+++ src/com/coding/arrays/remove_duplicates/RemoveDuplicates.java
@@ -1,5 +1,7 @@
 package com.coding.arrays.remove_duplicates;
 
+import java.util.Arrays;
+
 /**
  * Given a sorted array, remove the duplicates in place such that each element appear only once 
  * and return the new length. Do not allocate extra space for another array, 
@@ -11,23 +13,47 @@ package com.coding.arrays.remove_duplicates;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] array = {1,1,2};
-        System.out.println("hello world");
+        int[] A = {1,1,2,3,3,3,3};
+        System.out.println("Original Array: " + Arrays.toString(A));
+        // System.out.println("Res Array: " + Arrays.toString(getRemoveDuplicatesWhileLoop(A)));
+        System.out.println("Res For Loop: " + Arrays.toString(getRemoveDuplicatesForLoop(A)));
     }
-
-    public static int removeDuplicatesFromArray(int[] A) {
-        int i,j;
-        i=1;
-        j=0;
-
-        while(i < A.length) {
-            if(A[i] != A[j]) {
+    public static int[] getRemoveDuplicatesWhileLoop(int[] A) {
+        int i = 1, j = 0;
+        while (i < A.length) {
+            if (A[i] != A[j]) {
                 j++;
                 A[j] = A[i];
             }
-            i++; 
+            i++;
         }
 
-        return j+1;
+        int[] res = new int[j+1];
+        for (i = 0; i < res.length; i++) {
+            res[i] = A[i];
+        }
+
+        return res;
+    }
+
+    public static int[] getRemoveDuplicatesForLoop(int[] A) {
+        if (A == null || A.length == 0) {
+            throw new IllegalArgumentException("Illegal argument, null or array length 0");
+        }
+
+        int last = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[last] != A[i]) {
+                last++;
+                A[last] = A[i];
+            }
+        }
+
+        int[] res = new int[last + 1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = A[i];
+        }
+
+        return res;
     }
 }
